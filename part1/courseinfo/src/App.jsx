2a6e6286778b64eb24a274a0c307d3ac030d2a11
nColli <collinicolas01@gmail.com>
2024-01-04 16:37:23 -0300

    ex 1.1 and 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,44 @@
+/* eslint-disable react/prop-types */
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+/*
+const Content = (props) => {
+  return (
+    <p>{props.part} {props.exercises}</p>
+  )
+}
+*/
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.part[0]} exercise={props.exercise[0]}></Part>
+      <Part part={props.part[1]} exercise={props.exercise[1]}></Part>
+      <Part part={props.part[2]} exercise={props.exercise[2]}></Part>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+
+const Total = (props) => {
+  let total = 0
+  props.exercises.forEach(exercise => {
+    total += exercise
+  })
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,17 +50,9 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course}></Header>
+      <Content part={[part1,part2,part3]} exercise={[exercises1,exercises2,exercises3]}></Content>
+      <Total exercises={[exercises1,exercises2,exercises3]}></Total>
     </div>
   )
 }
