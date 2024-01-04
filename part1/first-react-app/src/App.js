66aa99424ee38165597742a11867523d3ee19e90
nColli <collinicolas01@gmail.com>
2024-01-04 09:24:57 -0300

    random sum component

--- part1/first-react-app/src/App.js
+++ part1/first-react-app/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Msj from './Msj'
+
+const SumRandoms = () => {
+  const a = Math.random()
+  const b = Math.random()
+  return (
+    <div>
+      <h2>Sum of random number: </h2>
+      <h3>{a} + {b} = {a+b}</h3>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Today is {new Date().toDateString()}</h1>
+      <Msj color="red"></Msj>
+      <SumRandoms></SumRandoms>
     </div>
   );
 }
