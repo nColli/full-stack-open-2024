7d2da4a4bd7b3c4f68849fffd239ae1c4053e7d1
nColli <collinicolas01@gmail.com>
2024-01-14 11:26:58 -0300

    courseinfo part 2

---
+++ part2/courseinfo-part2/src/App.jsx
@@ -0,0 +1,75 @@
+/* eslint-disable react/prop-types */
+const Header = (props) => {
+  console.log(props)
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return (
+    <div>
+      <Part part={props.parts[0].name} exercise={props.parts[0].exercises}></Part>
+      <Part part={props.parts[1].name} exercise={props.parts[1].exercises}></Part>
+      <Part part={props.parts[2].name} exercise={props.parts[2].exercises}></Part>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  console.log(props)
+  return (
+    <p>{props.part} {props.exercise}</p>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  let total = 0
+  props.parts.forEach(part => {
+    total += part.exercises
+  })
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  }
+
+  return <Course course={course} />
+}
+
+export default App
