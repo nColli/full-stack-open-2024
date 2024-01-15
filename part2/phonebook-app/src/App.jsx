28c1a38ccc8471f2ddcad6f9ff475ece9bdeaffd
nColli <collinicolas01@gmail.com>
2024-01-15 11:55:39 -0300

    phonebook 2.6 add names

--- part2/phonebook-app/src/App.jsx
+++ part2/phonebook-app/src/App.jsx
@@ -6,19 +6,48 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+    //console.log(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('new contact');
+
+    const personToAddToState = {
+      name: newName
+    };
+    console.log(personToAddToState);
+
+    setPersons(prevPersons => [
+      ...prevPersons,
+      personToAddToState
+    ])
+
+    setNewName('');
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input onChange={handleChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
-      ...
+      <div>
+        {persons.map(person =>
+          <p key={person.name}>{person.name}</p>  
+        )}
+      </div>
     </div>
   )
 }
