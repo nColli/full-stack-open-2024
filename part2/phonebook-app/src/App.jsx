a6951d25a585bcffa02fe56c7d639e31a86586a0
nColli <collinicolas01@gmail.com>
2024-01-16 10:31:41 -0300

    filter persons

--- part2/phonebook-app/src/App.jsx
+++ part2/phonebook-app/src/App.jsx
@@ -1,15 +1,27 @@
+/* eslint-disable react/prop-types */
 import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [nameFilter, setNameFilter] = useState('')
 
-  const handleChange = (event) => {
+  const handleChangeName = (event) => {
     setNewName(event.target.value)
     console.log(event.target.value);
   }
+
+  const handleChangeNumber = (event) => {
+    setNewNumber(event.target.value)
+    console.log(event.target.value);
+  }
+
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -17,12 +29,11 @@ const App = () => {
     console.log('new contact');
 
     const personToAddToState = {
-      name: newName
+      name: newName,
+      number: newNumber
     };
 
     console.log(personToAddToState);
-
-    
 
     let personsNames = []
 
@@ -35,7 +46,7 @@ const App = () => {
       alert(`${newName} is already added to phonebook`)
 
     } else {
-      console.log("add: ",newName);
+      console.log("add: ",newName,newNumber);
   
       setPersons(prevPersons => [
         ...prevPersons,
@@ -44,15 +55,47 @@ const App = () => {
     }
 
     setNewName("");
+    setNewNumber("");
   }
+
+  const handleFilter = (event) => {
+    const name = event.target.value
+    setNameFilter(name)
+  }
+
+  const Persons = ({persons,nameFilter}) => {
+    if (!(nameFilter === '')) {
+      console.log("search:",nameFilter);
+
+      let personsFinded = []
+
+      persons.map((person) => {if(person.name.includes(nameFilter)) { personsFinded.push(person) }})
+
+      persons = personsFinded; //overwriting para tener solo un return
+    }
+
+    return (
+      <div>
+        {persons.map(person =>
+          <p key={person.name}>{person.name} {person.number}</p>  )}
+      </div>
+    )
+  }
+
 
   return (
     <div>
       <h2>Phonebook</h2>
 
+      <p>filter shown with<input onChange={handleFilter}/></p>
+
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input onChange={handleChange} value={newName}/>
+          name: <input onChange={handleChangeName} value={newName}/>
+        </div>
+        <div>
+          number: <input onChange={handleChangeNumber} value={newNumber}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -60,13 +103,19 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      <div>
-        {persons.map(person =>
-          <p key={person.name}>{person.name}</p>  
-        )}
-      </div>
+      <Persons persons={persons} nameFilter={nameFilter}></Persons>
     </div>
   )
 }
 
 export default App
+
+/*
+
+<div>
+        {nameFilter===('') ? persons.map(person =>
+          <p key={person.name}>{person.name} {person.number}</p>  
+        ) : showPersonsFilter}
+
+      </div>
+*/
