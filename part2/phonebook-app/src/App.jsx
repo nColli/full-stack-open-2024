80f3b6efb4cb94124d07046a8c01a70bd8f87bb5
nColli <collinicolas01@gmail.com>
2024-01-18 13:00:44 -0300

    phonebook with useEffect and Axios

--- part2/phonebook-app/src/App.jsx
+++ part2/phonebook-app/src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Filter = ({handleFilter}) => {
   return <p>filter shown with<input onChange={handleFilter}/></p>
@@ -48,15 +49,22 @@ const Persons = ({persons,nameFilter}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
+
+  
+  useEffect(() => {
+    console.log("use effect");
+    
+    axios
+      .get('http://localhost:3001/persons')
+      .then((Response) => {
+        const { data } = Response;
+        setPersons(data)
+      })
+  }, [])
 
   const handleChangeName = (event) => {
     setNewName(event.target.value)
