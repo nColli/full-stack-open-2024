4fc5dd04f75e3e7a62feee204992009474687450
nColli <collinicolas01@gmail.com>
2024-01-21 12:41:02 -0300

    template notes-form-server-v2

---
+++ part2/notes-form-server-v2/src/App.jsx
@@ -0,0 +1,64 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
+import {Note} from './components/Note' 
+
+function App({ initialNotes = [] }) {
+  const [notes, setNotes] = useState(initialNotes)
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  const handleChange = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log('crear nota');
+    const noteToAddToState = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    };
+    console.log(noteToAddToState);
+
+    //setNotes(notes.concat(noteToAddToState))
+    setNotes(prevNotes => [
+      ...prevNotes,
+      noteToAddToState
+    ]);
+
+    setNewNote("");
+  }
+
+  const handleShowAll = () => {
+    setShowAll(() => !showAll)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <button onClick={handleShowAll}>
+        {showAll ? "Show only important" : "Show all"}
+      </button>
+      <ol>
+        {notes
+          .filter((note) => {
+            if (showAll === true) return true;
+            return note.important === true 
+          })
+          .map(note => 
+            <Note key={note.id} note={note} />
+          )}
+      </ol>
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={newNote}/>
+        <button>Submit note</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
