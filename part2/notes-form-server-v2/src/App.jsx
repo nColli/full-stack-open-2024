2116d9f75c7d57807985a7850f916fd11b88dca8
nColli <collinicolas01@gmail.com>
2024-01-22 17:17:53 -0300

    change notes's importance

--- part2/notes-form-server-v2/src/App.jsx
+++ part2/notes-form-server-v2/src/App.jsx
@@ -73,6 +73,19 @@ function App() {
       })
   }
 
+  const toggleImportanceOf = (id) => {
+    console.log(`importance of ${id} needs to be toggled`);
+
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
+
   const handleShowAll = () => {
     setShowAll(() => !showAll)
   }
@@ -90,7 +103,7 @@ function App() {
             return note.important === true 
           })
           .map(note => 
-            <Note key={note.id} note={note} />
+            <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
           )}
       </ol>
       <form onSubmit={addNote}>
