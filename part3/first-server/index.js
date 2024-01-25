f9eca381673d206b2d17cd40cf0c9c6aa10cf9c5
nColli <collinicolas01@gmail.com>
2024-01-25 10:24:18 -0300

    fetch a single resource and delete

--- part3/first-server/index.js
+++ part3/first-server/index.js
@@ -31,6 +31,23 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  const note = notes.find(note => {
+    //console.log(note.id, typeof note.id, id, typeof id, note.id === id);
+
+    return note.id === id
+  })
+  
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
