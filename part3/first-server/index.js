aef41395dc79976fb91f4fa2700042a4f48ca4c8
nColli <collinicolas01@gmail.com>
2024-01-25 12:25:52 -0300

    receive data v2

--- part3/first-server/index.js
+++ part3/first-server/index.js
@@ -56,13 +56,30 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-app.post('/api/notes', (request, response) => {
+const generateId = () => {
+  console.log('notes id with map', (notes.map(n => n.id)));
+
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
     : 0
 
-  const note = request.body
-  note.id = maxId + 1
+  return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
 
   notes = notes.concat(note)
 
