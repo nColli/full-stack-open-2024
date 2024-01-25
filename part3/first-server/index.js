db149106b3749d4f274eb44a48926cbb8cbab0e8
nColli <collinicolas01@gmail.com>
2024-01-25 11:48:54 -0300

    receive data post method notes

--- part3/first-server/index.js
+++ part3/first-server/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let notes = [
   {    
     id: 1,    
@@ -45,6 +47,27 @@ app.get('/api/notes/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+
+  const note = request.body
+  note.id = maxId + 1
+
+  notes = notes.concat(note)
+
+  console.log(note);
+  response.json(note)
 })
 
 
