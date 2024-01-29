11a5156058ac3508fbd5edf0fb940a4acb5520dc
nColli <collinicolas01@gmail.com>
2024-01-29 15:21:19 -0300

    mongoDB

--- part3/phonebook-app/src/services/persons.js
+++ part3/phonebook-app/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/persons'
+const baseUrl = 'https://phonebook-app-q9p8.onrender.com/api/persons'
 
 const getAll = () => {
     return (
